7397ce8c50c988bfc9fd4d8a1f738401ba3e5d60
Ibrahim Ahmad <ibrahim.ahmad58@gmail.com>
2022-12-04 13:52:52 +0700

    push update class

---
+++ Upload.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Repositories;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Storage;
+use Ixudra\Curl\Facades\Curl;
+
+class UploadRepository {
+    public function __construct()
+    {}
+
+    public function upload(Request $request, $name)
+    {
+            $avatar = $request->file($name);
+            $avatarName = time().'.'.$avatar->getClientOriginalExtension();
+            $putFile = Storage::putFileAs('avatars', $avatar, $avatarName, 'public');
+            $path = $putFile;
+            return $path;
+    }
+}
